7e488174b69ffd330f21ad36aa6af96a9a61184f
Desside <desside.programming@gmail.com>
2023-09-07 21:03:38 +0300

    commit 0.01

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, ConfigModule.forRoot({
+      envFilePath: 'env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
