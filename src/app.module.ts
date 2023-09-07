37f5133a35dd650b1a773a8ca709ff139a2e234c
Desside-Developer <desside.programming@gmail.com>
2023-09-07 21:25:49 +0300

    commit 0.02 TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
+
 @Module({
-  imports: [TestModule, ConfigModule.forRoot({
+  imports: [
+    TestModule,
+    ConfigModule.forRoot({
       envFilePath: 'env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
     }),
   ],
   controllers: [AppController],
