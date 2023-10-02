bf9bb7a584a9014d1b3618b12093a5495baddb55
Desside-Developer <desside.programming@gmail.com>
2023-10-02 03:14:36 +0300

    user-api-module-01

--- src/UserModule/user.controller.ts
+++ src/UserModule/user.controller.ts
@@ -1,24 +1,33 @@
-import { Body, Controller, Get, Param, Post, Delete } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './interface/user';
+import { UserDto, UserParamsDto } from './dto/user.dto';
+
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {
 
   }
-  // HTTP GET /USERS
+  // HTTP GET /user
   @Get()
-  GetUsers(): User[] {
+  getUsers(): User[] {
     return this.userService.getUsers();
+  }
+  @Get()
+  getUser(@Param() param: UserParamsDto): User {
+    return this.userService.getUser(param.email);
   }
   // HTTP POST /USER
   @Post()
+  @UsePipes(new ValidationPipe(
+
+  ))
   PostUser(@Body() user: User): User {
     return this.userService.addUser(user);
   }
   // HTTP DELETE /users/EMAIL@GMAIL.COM
   @Delete('/:email')
-  DeleteUser(@Param('email') email: string): User[] {
-    return this.userService.deleteUser(email);
+  DeleteUser(@Param() params: UserParamsDto): User[] {
+    return this.userService.deleteUser(params.email);
   }
 }
--- src/UserModule/user.service.ts
+++ src/UserModule/user.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './interface/user';
 
 @Injectable()
 export class UserService {
-  public users: User[];
+  public users: User[] = [];
   getUsers(): User[] {
     return this.users;
   }
   getUser(email: string): User {
-    return this.users.filter(i=> i.email === email)[0]
+    const userData = this.users.filter(i=> i.email === email);
+    if(userData && Array.isArray(userData) && userData.length > 0){
+      return userData[0];
+    }
+    throw new NotFoundException('User not found');
   }
-  // push error cannot read properties of underfined (reading 'push')
   addUser(user: User): User {
-    this.users.push();
+    this.users.push(user);
     return user;
   }
   deleteUser(email: string): User[] {
